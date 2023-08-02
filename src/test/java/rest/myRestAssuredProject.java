cd876eaef4e495873bda72230c234a072ff1bcdc
aya78 <ayak77431@gmail.com>
2023-08-02 17:51:11 +0300

    first commit

--- src/test/java/rest/myRestAssuredProject.java
+++ src/test/java/rest/myRestAssuredProject.java
@@ -1,7 +1,5 @@
 package rest;
 
-import io.restassured.matcher.RestAssuredMatchers.*;
-import org.hamcrest.Matchers.*;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
@@ -9,10 +7,10 @@ import java.util.concurrent.TimeUnit;
 import static io.restassured.RestAssured.*;
 //import static sun.net.ProgressMonitor.pm;
 
-public class myFirstRestAssured {
+public class myRestAssuredProject {
    final static String url = "http://demo.guru99.com/V4/sinkministatement.php";
-    @Test
-    public static void  myFirstRestAssuredClass(){
+    @Test(priority = 0)
+    public static void  getResponseBody(){
 //        given().baseUri("http://demo.guru99.com/V4/sinkministatement.php?CUSTOMER_ID=68195&PASSWORD=1234!&Account_No=1").then().log()
 //                .all();
 
@@ -22,7 +20,7 @@ public class myFirstRestAssured {
                 .when().get("http://demo.guru99.com/V4/sinkministatement.php").then().log()
                 .body();
     }
-    @Test
+    @Test(priority = 1)
     public static void getResponseStatus(){
         int status_code = given().queryParam("CUSTOMER_ID","68195")
                 .queryParam("PASSWORD","1234!")
@@ -31,14 +29,19 @@ public class myFirstRestAssured {
         System.out.println("The response status is "+status_code);
         given().when().get(url).then().assertThat().statusCode(200);
     }
-    @Test
+    @Test(priority = 2)
     public static void getResponseHeader(){
 
         System.out.println("the header in the response"+ get(url).then().extract().headers());
     }
-    @Test
+    @Test(priority = 3)
     public static void getResponseTime(){
 
         System.out.println("the Time in the response"+ get(url).timeIn(TimeUnit.MILLISECONDS)+" MILLISECONDS");
+    }
+    @Test(priority = 4)
+    public static void getResponseContentType(){
+
+        System.out.println("the content type of response "+get(url).then().extract().contentType());
     }
 }
